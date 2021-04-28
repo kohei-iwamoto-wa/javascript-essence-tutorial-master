c02b11414b6db4a14c4d7c70feee8e1c642d6e13
kohei-iwamoto-wa <bananadesu9@gmail.com>
2021-04-28 18:46:11 +0900

    060_クロージャー動的な関数

---
+++ 030_スコープ/060_クロージャー（動的な関数生成）/start/main.js
@@ -0,0 +1,11 @@
+function addNumberFactory(num) {
+    function addNumber(value) {
+        return num + value;
+    }
+    // 関数を返却することができる
+    return addNumber;
+}
+
+const add5 = addNumberFactory(5);
+const result = add5(10);
+console.log(result);
