515546ccccd19a5a9f0c99ac1c1ecfa5fe2b762d
kohei-iwamoto-wa <bananadesu9@gmail.com>
2021-05-18 17:20:46 +0900

    010_let const var

---
+++ 040_変数/010_let,constとvar/start/main.js
@@ -0,0 +1,22 @@
+// letは再宣言できない
+// let a = 0;
+// let a = 0;
+
+// var b = 0;
+// var b = 1;
+
+// 再代入OK
+// let c = 0;
+// c = 2;
+
+// 再代入できない
+// const d = 0;
+// d = 1;
+
+{
+    let e = 0;
+    var f = 0;
+}
+
+let g = 0;
+var h = 0;
